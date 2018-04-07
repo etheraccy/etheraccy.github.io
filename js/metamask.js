0241d8f804d43cf4ad6d222b739a1964248a1cfd
etheraccy <37975232+etheraccy@users.noreply.github.com>
2018-04-07 18:56:44 +0100

    Create metamask.js

---
+++ js/metamask.js
@@ -0,0 +1,29 @@
+const metamask = function() {
+  
+  function init() {
+    setInterval(function() {
+      web3.eth.getAccounts(function (err, accounts) {
+        if(err) return
+        console.log(accounts);
+        let element = document.getElementById("metamask");
+        if(accounts[0]) {
+          element.style.color = "green";
+          element.textContent = "Metamask Unlocked";
+          localStorage.setItem("userAddress",accounts[0]);
+        }
+        else {
+          element.style.color = "red";
+          element.textContent = "Metamask Locked";
+        } 
+        
+      })
+      
+    }, 5000)
+    
+  }
+
+  return {
+    init
+  }
+  
+}()  
