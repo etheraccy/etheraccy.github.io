dce34f4a52f3bcbc17dbbbc4dde13599b247cdd5
etheraccy <37975232+etheraccy@users.noreply.github.com>
2018-04-09 16:22:54 +0100

    Update getLink.js

--- js/getLink.js
+++ js/getLink.js
@@ -2,7 +2,7 @@ const getLink = function() {
   
   function init() {
     if(window.location.href === "https://etheraccy.github.io/gamePage.html") { 
-      let order = decodeURI(localStorage.setItem("order",orderArr[1]));
+      let order = decodeURI(localStorage.getItem("order"));
       let URL = "https://etheraccy.github.io/?" + order; 
       $('#gameLink').text(URL);
     }  
