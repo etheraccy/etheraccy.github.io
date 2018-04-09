0bcaa776abfea257f7d41213de575ebb1e6e31ac
etheraccy <37975232+etheraccy@users.noreply.github.com>
2018-04-09 13:08:45 +0100

    Update EtherAccy.js

--- js/EtherAccy.js
+++ js/EtherAccy.js
@@ -3,7 +3,8 @@ const EtherAccy = function() {
   function setBalance(value) {
     let balance = parseInt(value)/Math.pow(10,18);
     $('body > div > div > div > div.row.tp > div:nth-child(1) > div > div:nth-child(2) > input').attr('placeholder',balance);
-    $('body > div > div > div > div.row.tp > div:nth-child(2) > div > div:nth-child(2) > input').attr('placeholder',balance);    
+    $('body > div > div > div > div.row.tp > div:nth-child(2) > div > div:nth-child(2) > input').attr('placeholder',balance);
+    $('body > div > div > div > div.header > div:nth-child(1) > div > div.meta.float-right.text-center > h5:nth-child(2) > span').text(balance);
   }  
   
   function getBalance() {
