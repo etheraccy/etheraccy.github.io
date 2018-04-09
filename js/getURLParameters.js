5a39afd75acf44554e2849e9444fbcb08e0a9b5d
etheraccy <37975232+etheraccy@users.noreply.github.com>
2018-04-09 15:44:06 +0100

    Update getURLParameters.js

--- js/getURLParameters.js
+++ js/getURLParameters.js
@@ -3,7 +3,7 @@ const getParameters = function() {
   function init() {
     let orderArr = window.location.href.split("?");
     if(orderArr.length > 1) 
-    localStorage.setItem("order",orderArr[1]);
+    localStorage.setItem("order",encodeURI(orderArr[1]));
   }
 
   return {
