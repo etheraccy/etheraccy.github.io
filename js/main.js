f08115983d7d638a5d62a754b9cb6d9630005d2e
etheraccy <37975232+etheraccy@users.noreply.github.com>
2018-04-09 16:41:23 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,4 +2,7 @@ window.onload = function () {
   metamask.init();
   getParameters.init();
   EtherAccy.init();
+  if(window.location.href === "https://etheraccy.github.io/gamePage.html") { 
+    console.log(contractFunctions.getGameStruct());
+  }
 };  
