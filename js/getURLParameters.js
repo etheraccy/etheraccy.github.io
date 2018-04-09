c686a7ebf5cb656cfebb46e31a32e6233f8f16b9
etheraccy <37975232+etheraccy@users.noreply.github.com>
2018-04-09 15:49:00 +0100

    Update getURLParameters.js

--- js/getURLParameters.js
+++ js/getURLParameters.js
@@ -2,8 +2,10 @@ const getParameters = function() {
   
   function init() {
     let orderArr = window.location.href.split("?");
-    if(orderArr.length > 1) 
-    localStorage.setItem("order",orderArr[1]);
+    if(orderArr.length > 1) { 
+      localStorage.setItem("order",orderArr[1]);
+      window.location.href = "https://etheraccy.github.io/gamePage.html";
+      console.log(decodeURI(localStorage.getItem("order")));
   }
 
   return {
