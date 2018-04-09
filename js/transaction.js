278f7c68847f2100b46b477894e840e555296fb6
etheraccy <37975232+etheraccy@users.noreply.github.com>
2018-04-09 14:17:44 +0100

    Update transaction.js

--- js/transaction.js
+++ js/transaction.js
@@ -1,10 +1,10 @@
 const transaction = function() {
 
-  function send(Tx,callback) {
+  function send(Tx,callback,args) {
     web3.eth.sendTransaction(Tx, function(err,transactionHash) {
       if (err || !transactionHash) return;
       if(callback) {
-        callback();
+        callback(args);
       }  
     });
   }
