64493c7a79c153e4f226385156a0d281a3c5c4ef
etheraccy <37975232+etheraccy@users.noreply.github.com>
2018-04-09 13:46:58 +0100

    Update contractFunctions.js

--- js/contractFunctions.js
+++ js/contractFunctions.js
@@ -12,10 +12,60 @@ const contractFunctions = function() {
       callback(val);  
     })    
   }
+ 
+  function toFixedNumber(i) {
+    if(~i.toString().indexOf("e")) {
+        let num = parseInt(i);
+        while(i > 1) {
+            num += "0";
+            i /= 10;
+        }
+        return num;
+    }
+    return i.toString();
+  }
+  
+  function keccak256(...args) {
+    args = args.map(arg => {
+      if (typeof arg === 'string') {
+         if (arg.substring(0, 2) === '0x') {
+           return arg.slice(2)
+        } else {
+            return web3.toHex(arg).slice(2)
+        }
+      }
+
+      if (typeof arg === 'number') {
+        return leftPad((arg).toString(16), 64, 0)
+      } else {
+        return ''
+      }
+    })
+
+    args = args.join('')
+
+    return web3.sha3(args, { encoding: 'hex' })
+  }
+  
+  function create(callback) {
+    let userAddress = localStorage.getItem("userAddress");    
+    let ante = $('body > div > div > div > div.row.tp > div:nth-child(2) > div > div:nth-child(2) > input').val();
+    let deadline = $('body > div > div > div > div.row.tp > div:nth-child(2) > div > div:nth-child(3) > input').val();
+    let betWindow = $('body > div > div > div > div.row.tp > div:nth-child(2) > div > div:nth-child(4) > input').val();
+    let nonce = Math.random().toString().slice(2);
+    let data = contract.createGame.getData(ante,deadline,betWindow,nonce);
+    let Tx = {
+       from: userAddress,
+       to: contractAddress,
+       data: data,
+       gasPrice: gasPrice
+    };
+    transaction.send(Tx,callback);      
+  }
   
   function deposit(value) {
     let userAddress = localStorage.getItem("userAddress");
-    let ethValue = parseInt(parseFloat(value)*Math.pow(10,18));
+    let ethValue = toFixedNumber(parseFloat(value)*Math.pow(10,18));
     let data = contract.deposit.getData();
     let Tx = {
        from: userAddress,
@@ -29,7 +79,7 @@ const contractFunctions = function() {
   
   function withdraw(value) {
     let userAddress = localStorage.getItem("userAddress");
-    let ethValue = parseInt(parseFloat(value)*Math.pow(10,18));    
+    let ethValue = toFixedNumber(parseFloat(value)*Math.pow(10,18));    
     let data = contract.withdraw.getData(ethValue);
     let Tx = {
        from: userAddress,
@@ -46,6 +96,7 @@ const contractFunctions = function() {
 
   return {
     getBalance,
+    create,
     deposit,
     withdraw
   };
