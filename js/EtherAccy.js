080fd6f310b3e17edd52a1a2e7be5640c71201aa
etheraccy <37975232+etheraccy@users.noreply.github.com>
2018-04-12 12:24:55 +0100

    Update EtherAccy.js

--- js/EtherAccy.js
+++ js/EtherAccy.js
@@ -212,11 +212,13 @@ const EtherAccy = function() {
   function selectMinBet() {
     $('#bettorPage > div > ul > div.col-lg-9 > li.min > h5 > span').addClass('selectedBox');
     $('#bettorPage > div > ul > div.col-lg-9 > li.max > h5 > span').removeClass('selectedBox');
+    $('#bettorPage > div > ul > div.col-lg-9 > li.cus > h5 > span').css('border-color','#afafaf');  
   }  
   
   function selectMaxBet() {
     $('#bettorPage > div > ul > div.col-lg-9 > li.max > h5 > span').addClass('selectedBox');
-    $('#bettorPage > div > ul > div.col-lg-9 > li.min > h5 > span').removeClass('selectedBox'); 
+    $('#bettorPage > div > ul > div.col-lg-9 > li.min > h5 > span').removeClass('selectedBox');
+    $('#bettorPage > div > ul > div.col-lg-9 > li.cus > h5 > span').css('border-color','afafaf');      
   }
   
   function selectCustomBet() {
