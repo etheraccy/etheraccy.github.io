7769eefcf363cbfffb77c2bb94a2ae92aa0d1e14
etheraccy <37975232+etheraccy@users.noreply.github.com>
2018-04-13 23:49:23 +0100

    Update EtherAccy.js

--- js/EtherAccy.js
+++ js/EtherAccy.js
@@ -250,7 +250,7 @@ const EtherAccy = function() {
     let cardType = localStorage.getItem(cardTypeNum);
     while(cardType === null) {
       cardTypeNum = Math.random % 4;
-      cardType = localStorage.getItem(cardTypeNum);
+      cardType = localStorage.setItem(cardTypeNum);
     }  
     if(cardType < 12) {
       localStorage.setItem(cardTypeNum,cardType++);
