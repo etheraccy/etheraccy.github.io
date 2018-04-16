461a823713e9fe34beb7dc07a4834022c6cc8fba
etheraccy <37975232+etheraccy@users.noreply.github.com>
2018-04-16 13:32:18 +0100

    Update EtherAccy.js

--- js/EtherAccy.js
+++ js/EtherAccy.js
@@ -105,7 +105,7 @@ const EtherAccy = function() {
             $('#bettorPage > ul').show();           
             $('#placeBet').show();
         }  
-        else if(currentPlayer !== userAddress) {
+        if(currentPlayer !== userAddress) {
           $('#mainGamePage').show();          
           $('#mainGamePage > ul').show();          
         }  
