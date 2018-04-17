895f07c19a00be9b47575f47a68cfcabd96756c9
etheraccy <37975232+etheraccy@users.noreply.github.com>
2018-04-17 12:36:53 +0100

    Update EtherAccy.js

--- js/EtherAccy.js
+++ js/EtherAccy.js
@@ -307,9 +307,8 @@ const EtherAccy = function() {
       betValue = parseFloat($('#potAmount').text().replace(" ETH",""))*1e18;
     }  
     else if(betChoice === "customBet") {
-      betValue = parseFloat($('#bettorPage > div > ul > div.col-lg-9 > li.cus > h5 > span > input').val());
+      betValue = parseFloat($('#bettorPage > div > ul > div.col-lg-9 > li.cus > h5 > span > input').val())*1e18;
     }
-    console.log(betValue);
     contractFunctions.bet(betValue);
   }  
  
