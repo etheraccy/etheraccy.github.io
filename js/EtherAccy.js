0e7570a3b8c92727ab310ad55ca0afb9bd723013
etheraccy <37975232+etheraccy@users.noreply.github.com>
2018-04-17 12:47:36 +0100

    Update EtherAccy.js

--- js/EtherAccy.js
+++ js/EtherAccy.js
@@ -301,13 +301,13 @@ const EtherAccy = function() {
     localStorage.removeItem("betChoice");   
     let betValue;
     if(betChoice === "minBet") {
-      betValue = parseFloat($('#minBet').text().replace(" ETH",""))*1e18;
+      betValue = (parseFloat($('#minBet').text().replace(" ETH",""))*1e18).toFixed(0);
     }  
     else if(betChoice === "maxBet") {
-      betValue = parseFloat($('#potAmount').text().replace(" ETH",""))*1e18;
+      betValue = (parseFloat($('#potAmount').text().replace(" ETH",""))*1e18).toFixed(0);
     }  
     else if(betChoice === "customBet") {
-      betValue = parseFloat($('#bettorPage > div > ul > div.col-lg-9 > li.cus > h5 > span > input').val())*1e18;
+      betValue = (parseFloat($('#bettorPage > div > ul > div.col-lg-9 > li.cus > h5 > span > input').val())*1e18).toFixed(0);
     }
     contractFunctions.bet(betValue);
   }  
